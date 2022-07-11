1199624bd1895558a71410e740b64ddefcb11ce6
SoleDalia <104218640+SoleDalia@users.noreply.github.com>
2022-07-11 17:38:40 -0300

    about button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import "./App.css";
 import AdminLogin from "./components/login/AdminLogin";
 import MySnackBar from "./components/snackBar/MySnackBar";
 import MyRoutes from "./MyRoutes";
+import { Box, Fab } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import NavigationIcon from "@mui/icons-material/Navigation";
 
 const myTheme = createTheme({
   palette: {
@@ -34,6 +37,7 @@ const myTheme = createTheme({
 
 function App() {
   const admin = useSelector((state) => state.admin);
+  const navigate = useNavigate();
   return (
     <ThemeProvider theme={myTheme}>
       <SnackbarProvider maxSnack={4}>
@@ -41,6 +45,26 @@ function App() {
           <>
             {Object.entries(admin).length === 0 ? <AdminLogin /> : <MyRoutes />}
             <MySnackBar />
+            <Box
+              sx={{
+                "& > :not(style)": { m: 1 },
+                position: "fixed",
+                top: "9vh",
+                right: "-5rem",
+                zIndex: "100",
+                transition: "0.3s",
+                "&:hover": {
+                  transition: "0.3s",
+                  right: 0,
+                },
+              }}
+              onClick={() => navigate("/about")}
+            >
+              <Fab variant="extended" color="secondary" aria-label="add">
+                <NavigationIcon sx={{ mr: 1 }} />
+                About
+              </Fab>
+            </Box>
           </>
         </div>
       </SnackbarProvider>
